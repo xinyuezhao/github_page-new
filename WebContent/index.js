84c87b3d526277fdaf867a17b15a78bfedce8b04
git <git@goodchinese.com>
2015-09-27 08:44:44 -0700

    add console.log

--- WebContent/index.js
+++ WebContent/index.js
@@ -18,7 +18,7 @@
                 var speed = offset/(time/inteval);
                 var list = document.getElementById('list');
                 var left = parseInt(list.style.left) + offset;
-                console.log("animate:"+offset+" style.left:"+list.style.left+" left:"+list);
+                console.log("animate:"+offset+" style.left:"+list.style.left+" left:"+left);
                 var go = function (){
                     if ( (speed > 0 && parseInt(list.style.left) < left) || (speed < 0 && parseInt(list.style.left) > left)) {
                         list.style.left = parseInt(list.style.left) + speed + 'px';
